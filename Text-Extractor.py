884ea67153a7f69f3a890d00f15c327fb72b75e8
Emma Markle <130492970+emmamarkle@users.noreply.github.com>
2025-02-06 22:21:39 -0500

    Update Text-Extractor.py

--- Text-Extractor.py
+++ Text-Extractor.py
@@ -1,7 +1,6 @@
 import re
-import os
 
-# Extracts the sentence from the given text files and return it in a list
+'''Extracts the sentence from the given text files and return it in a list given the files language and respective keyword'''
 def sentence_extractor(files, lang, keyword):
     sentences = []
     for file in files:
@@ -16,7 +15,7 @@ def sentence_extractor(files, lang, keyword):
                     sentences.append(sentence)
     return sentences
 
-# Extracts the sentence level UMR graph for the sentence from the given text files and return it in a list
+'''Extracts the sentence level UMR graph for the sentence from the given text files and return it in a list'''
 def sentence_umr_extractor(files):
     umr_graphs = []
     for file in files:
@@ -29,7 +28,7 @@ def sentence_umr_extractor(files):
                     umr_graphs.append(combined_lines)
     return umr_graphs
 
-# Extracts the alignment for the sentence from the given text file and return it in a list
+'''Extracts the alignment for the sentence from the given text file and return it in a list'''
 def alignment_extractor(files): 
     alignments = []
     for file in files:
@@ -44,7 +43,7 @@ def alignment_extractor(files):
                     alignments.append("") 
     return alignments
 
-# Extracts the document level UMR graph for the sentence from the given text file and return it in a list
+'''Extracts the document level UMR graph for the sentence from the given text file and return it in a list'''
 def doc_level_extractor(files):
     umr_graphs = []
     for file in files:
@@ -57,7 +56,7 @@ def doc_level_extractor(files):
                     umr_graphs.append(combined_lines)
     return umr_graphs
 
-# Combines two files in the order given
+'''Combines two files in the order given'''
 def combine_files(files, output):
     with open(output, 'w', encoding="utf-8") as outfile:
         for file in files:
@@ -65,21 +64,37 @@ def combine_files(files, output):
                 for line in infile:
                     outfile.write(line)
 
-# Combines the first lines of the given dictionaries in the format set to variable "combines"
+'''Combines the first lines of the given dictionaries in the format set to variable "combines"'''
 def combine(sentences, sentence_graphs, alignments, document_graphs, output):
     with open(output, 'w', encoding="utf-8") as outfile:
         for sentence, sentgraph, align, docgraph in zip(sentences, sentence_graphs, alignments, document_graphs):
+            ''' Change the line below to change the format in which the data should be formatted '''
+            ''' NOTE: SEE BOTTOM OF FILE TO SEE FORMAT OF MODELS USED '''
             combined = sentence + '\tSENTENCE ' + sentgraph + ' ALIGNMENT ' + align + ' DOCUMENT ' + docgraph + '\n'
             outfile.write(combined)
 
-# Given a list and output file, writes list item by item and returns afetr each item
+'''Given a list and output file, writes list item by item and returns after each item'''
 def write_to_file(list, output):
     with open(output, 'w', encoding="utf-8") as outfile:
         for item in list:
                 outfile.write(item + '\n')
 
+'''
+FORMAT OF AMRLIB:
+# ::id
+# ::snt (sentence)
+# ::save-date
+(sentence level graph) (alignment) (document level graph)
 
+Value of "combined" for the method combine DOCUMENT LEVEL:
+combined = '# ::id\n# ::snt' + sentence + '\n' + '# ::save-date\n' + sentgraph + align + docgraph + '\n'
 
+Value of "combined" for the method combine SENTENCE LEVEL:
+combined = '# ::id\n# ::snt' + sentence + '\n' + '# ::save-date\n' + sentgraph
+'''
 
-
-
+'''
+FORMAT OF UMR TO AMR CONVERSION:
+NOTE: the UMR to AMR conversion pipeline can only be ran on UMR sentence level data
+combined = (sentence level UMR GRAPH)
+'''
